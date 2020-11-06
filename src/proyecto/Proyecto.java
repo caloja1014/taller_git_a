a327e8e7f1debba0fd34a294e60d161b667c13d3
Neoterux <labajana@espol.edu.ec>
2020-11-05 19:01:53 -0500

    commit asd

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -17,6 +17,8 @@ public class Proyecto {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Hola mundo2");
+        
+        System.out.println("Hola mundo, push #1");
     }
     
 }
